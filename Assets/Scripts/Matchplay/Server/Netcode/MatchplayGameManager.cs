5f840524b433abe515206bd7dd8f09e367ea8129
Batuhan Özdemir <b.ozdmr23@gmail.com>
2023-11-04 13:48:31 +0300

    added game manager structure

---
+++ Assets/Scripts/Matchplay/Server/Netcode/MatchplayGameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Unity.Netcode;
+
+
+namespace Matchplay.Server
+{
+    public enum TeamName 
+    {
+        Red,
+        Blue,
+        Green,
+        Yellow
+    } 
+    
+    public class Team
+    {
+        private List<Matchplayer> Players;
+        
+        public Team()
+        {
+            Players = new List<Matchplayer>();
+        }
+    
+        public void AddPlayer(Matchplayer player)
+        {
+            Players.Add(player);
+        }
+    }
+
+    public class Match
+    {
+        public Dictionary<TeamName, Team> Teams;
+
+        public Match(TeamName teamName, Team team)
+        {
+            
+        }
+        
+    }
+    public class MatchplayGameManager : NetworkBehaviour
+    {
+        
+    }
+}
